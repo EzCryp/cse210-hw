4ae85e1e1f35b2a9259caf4e280ee7bcf0986ac2
Ezcryp <u637424283@byui.edu>
2024-05-05 13:34:12 +0800

    Prep 1 Assignment

---
+++ prepare/Learning02/Entry.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Learning02
+{
+    class Entry
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your favorite color? ");
+            string color = Console.ReadLine();
+
+            // Rest of your code goes here
+
+        }
+    }
+}
