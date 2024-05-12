157ef355dcfa6b6ab9c08cb4bcce3681525862aa
Ezcryp <u637424283@byui.edu>
2024-05-13 00:33:14 +0800

    resume

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -3,13 +3,16 @@ using System.Data; // for System.Data
 
 public class Job
 {
-    // attributes
+    // attributes of a job (like jobtitle, company, start year, and end year)
+    // responbilities
     public string _company;
     public string _jobTitle;
     public int _startYear;
     public int _endYear;
 
-    public void Display()
+    // behaviors
+    // ex. Software Engineer (Microsoft) 2019-2022
+    public void DisplayJobDetails()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
